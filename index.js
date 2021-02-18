4b4e222647e08a8760b743b9b9cdfa881f6b9035
Mfitumukiza Peter <mfitep6@gmail.com>
2021-02-18 16:44:17 +0200

    the real commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require("./model/mongo");
+const express = require('express');
+const bodyParser = require('body-parser');
+const provinceController = require("./controller/provinceController");
+const districtControler = require("./controller/districtController");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
+
+app.use("/provinces",provinceController);
+app.use("/districts",districtControler);
+
+app.listen(3000,()=>{
+    res.send("Running");
+});
